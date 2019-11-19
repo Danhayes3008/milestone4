38211df42cb71ca62aa5070bc3685a21e7f1f523
danhayes <danhayes3008@outlook.com>
2019-11-19 17:50:32 +0000

    created the urls.py file to handle the views

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url, include
+from users.views import login, logout, profile, registration
+
+urlpatterns = [
+    url(r'^login/', login, name="login"),
+    url(r'^logout/', logout, name='logout'),
+    url(r'^profile/', profile, name='profile'),
+    url(r^'register/', registration, name='register')
+]
