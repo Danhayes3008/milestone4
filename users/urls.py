4092653624116caf4e0387a6cc4ae1859a108013
danhayes <danhayes3008@outlook.com>
2019-11-19 17:54:45 +0000

    fixed a typo on line 8

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'^login/', login, name="login"),
     url(r'^logout/', logout, name='logout'),
     url(r'^profile/', profile, name='profile'),
-    url(r^'register/', registration, name='register')
+    url(r'^register/', registration, name='register')
 ]
