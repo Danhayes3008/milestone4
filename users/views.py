a1ff6c5c189f2d41febe589f911de9468fad6902
danhayes <danhayes3008@outlook.com>
2019-11-19 16:48:32 +0000

    added the login def to the file so that users can login

--- users/views.py
+++ users/views.py
@@ -7,3 +7,25 @@ from users.forms import LoginForm, RegistrationForm
 # This def will show us the index.html page 
 def index(request):
     return render(request, 'index.html')
+
+# This def creates the login capability and uses the login form from the forms.py file
+
+def Login(request):
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
+    if request.method == "POST":
+        login_form = LoginForm(request.POST)
+        
+        if login_form.is_valid():
+            user = auth.authenticate(username=request.POST['username'],
+                                     password=request.POST['password'])
+            messages.success(request, "Welcome to Help the Homeless!")
+            
+            if user:
+                auth.login(user=user, request=request)
+                return redirect(reverse('index'))
+            else:
+                login_form.add_error(None, "Incorrect username or password, please try again.")
+    else:
+        login_form = LoginForm()
+    return render(request, 'login.html', {'login_form': login_form})
