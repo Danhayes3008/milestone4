816eb806872f1af4867b1e7b6b157bd10dc1b992
danhayes <danhayes3008@outlook.com>
2019-11-19 16:37:37 +0000

    started working on the views file by adding all the imports needed and adding the def for the index

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from users.forms import LoginForm, RegistrationForm
 
-# Create your views here.
+# This def will show us the index.html page 
+def index(request):
+    return render(request, 'index.html')
