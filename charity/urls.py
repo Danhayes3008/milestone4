52785181833a0d89aeedf2d82c6417de2f16355a
danhayes <danhayes3008@outlook.com>
2019-11-19 18:02:32 +0000

    added the url information for the users app to the main urls.py file

--- charity/urls.py
+++ charity/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from users.views import index
+from users import urls as urls_users
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^users/', include(urls_users))
 ]
