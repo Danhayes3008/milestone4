07a8f018ff9670b7b1b7853c7413e00c9a06f765
danhayes <danhayes3008@outlook.com>
2019-11-21 12:47:32 +0000

    fixed the users.views imports

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url, include
-from users.views import Login, logout, profile, registration
+from users.views import login, logout, profile, registration
 
 urlpatterns = [
-    url(r'^login/', Login, name="login"),
+    url(r'^login/', login, name="login"),
     url(r'^logout/', logout, name='logout'),
     url(r'^profile/', profile, name='profile'),
     url(r'^register/', registration, name='register')
