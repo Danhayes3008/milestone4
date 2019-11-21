ce0be2a4c7cd113255c75277281252237a1ecced
danhayes <danhayes3008@outlook.com>
2019-11-21 11:04:06 +0000

    fixed error were forms was imported wrongly, Login and Registration had Form at the end of there names when they shouldnt have. fixed same problem on lines 17 and 30

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from users.forms import LoginForm, RegistrationForm
+from users.forms import Login, Registration
 
 # This def will show us the index.html page 
 def index(request):
@@ -14,7 +14,7 @@ def Login(request):
     if request.user.is_authenticated:
         return redirect(reverse('index'))
     if request.method == "POST":
-        login_form = LoginForm(request.POST)
+        login_form = Login(request.POST)
         
         if login_form.is_valid():
             user = auth.authenticate(username=request.POST['username'],
@@ -27,7 +27,7 @@ def Login(request):
             else:
                 login_form.add_error(None, "Incorrect username or password, please try again.")
     else:
-        login_form = LoginForm()
+        login_form = Login()
     return render(request, 'login.html', {'login_form': login_form})
 
 # This section deals with the logout function
