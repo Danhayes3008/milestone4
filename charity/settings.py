65523130e3d746e6b678925362a111d2b00be2fd
danhayes <danhayes3008@outlook.com>
2019-11-26 15:29:04 +0000

    added the projects and home apps to installed apps

--- charity/settings.py
+++ charity/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_forms_bootstrap',
-    'users'
+    'users',
+    'projects',
+    'home'
 ]
 
 MIDDLEWARE = [
