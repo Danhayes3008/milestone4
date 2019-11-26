0c74f34c2f2f2c057930b6303da15557c78c8de6
danhayes <danhayes3008@outlook.com>
2019-11-26 14:51:29 +0000

    made the Project class that will allow me to create projects for people to donate to. used default for most of it as this is a universal class that can create any type of product for donations

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Projects(models.Model):
+    name = models.CharField(max_length=254, default='')
+    description = models.TextField()
+    target = models.DecimalField(max_digits=9, decimal_places=2)
+    image = models.ImageField(uplaod_to='images')
+    AddressLine1 = models.CharField(max_length=50, default='')
+    AddressLine2 = models.CharField(max_length=50, default='')
+    town_or_city = models.CharField(max_length=50, default='')
+    county = models.CharField(max_length=50, default='')
+    country = models.CharField(max_length=50, default='')
+    postcode = models.CharField(max_length=20, default='')
+    
+    def __str__(self):
+        return self.name
