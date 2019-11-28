aa07e334c4f8d13a4c233d7f15d63c71a3839c8e
danhayes <danhayes3008@outlook.com>
2019-11-28 11:56:50 +0000

    added the urls for the new pages

--- projects/urls.py
+++ projects/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url, include
-from .views import projects
+from .views import projects, housing, training, support
 
 urlpatterns = [
-    url(r'^$', projects, name="donation")
+    url(r'^$', projects, name="donation"),
+    url(r'^home/', housing, name="housing"),
+    url(r'^training', training, name="training"),
+    url(r'^support', support, name="support")
 ]
