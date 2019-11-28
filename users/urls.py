32fdf88aa701acdf31832ef5180ca6f4aeb06367
danhayes <danhayes3008@outlook.com>
2019-11-28 16:09:06 +0000

    updated to include the url pattern for url_reset

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url, include
-from users.views import login, logout, profile, registration
+from users.views import login, logout, profile, registration, profile
+from users import url_reset
 
 urlpatterns = [
     url(r'^login/', login, name="login"),
     url(r'^logout/', logout, name='logout'),
     url(r'^profile/', profile, name='profile'),
-    url(r'^register/', registration, name='register')
+    url(r'^register/', registration, name='register'),
+    url(r'^password-reset/', include(url_reset)),
 ]
