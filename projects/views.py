d2eb2a31ba6d7983e1bcc7d791d7513e6c8dbbf8
danhayes <danhayes3008@outlook.com>
2019-11-28 11:52:32 +0000

    added the reders for the housing, training and general support pages

--- projects/views.py
+++ projects/views.py
@@ -4,3 +4,12 @@ from .models import Projects
 def projects(request):
     donation = Projects.objects.all()
     return render(request, "projects.html", {"donation": donation})
+
+def housing(request):
+    return render(request, "housing.html")
+
+def training(request):
+    return render(request, "training.html")
+
+def support(request):
+    return render(request, "generalSupport.html")
