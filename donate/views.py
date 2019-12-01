ba4a9827a79a42174d1ac22e9b277824df06bdc9
danhayes <danhayes3008@outlook.com>
2019-12-01 13:40:03 +0000

    added the amount to donate def to the file. This should allow the user to enter the amount they wish to donate

--- donate/views.py
+++ donate/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render, redirect, reverse
 
 def view_donate(request):
     return render(request, "donate.html")
+
+def amount_to_donate(request):
+    total = int(request.POST.get('total'))
+    
+    donate = request.session.get('cart', {})
+    if id in donate:
+        donate[id] = int(donate[id]) + total
+    else:
+        donate[id] = donate.get(id, total)
+    
+    request.session['donate'] = donate
+    return redirect(reverse('index'))
