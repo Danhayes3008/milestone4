75e61a31894b53d6495374210f80462b517a0caf
danhayes <danhayes3008@outlook.com>
2019-12-06 11:21:05 +0000

    worked on the def for allowing me to add data to mysql relating to total homless people and total people in the area

--- about/views.py
+++ about/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import total_homeless
 
 def about(request):
     return render(request, "about.html")
+def Homeless(request):
+    homeless = total_homeless.objects.all()
+    return render(request, {"homeless": homeless})
