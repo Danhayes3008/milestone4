9bb751ec5947fa07aae1927f521895f228c1de19
danhayes <danhayes3008@outlook.com>
2019-12-06 12:06:11 +0000

    added the render for the yearly homless class

--- about/views.py
+++ about/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from .models import total_homeless
+from .models import total_homeless, yearly_homeless
 
 def about(request):
     return render(request, "about.html")
+
 def Homeless(request):
     homeless = total_homeless.objects.all()
     return render(request, {"homeless": homeless})
+
+def yearlyTotal(request):
+    total = yearly_homeless.objects.all()
+    return render(request, {"total": total})
