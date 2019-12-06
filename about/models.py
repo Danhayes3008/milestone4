3e205e5cc311a5096ed009c67026190381bf8a54
danhayes <danhayes3008@outlook.com>
2019-12-06 12:02:07 +0000

    created the model that will allow me to add the data for yearly homeless totals

--- about/models.py
+++ about/models.py
@@ -7,3 +7,11 @@ class total_homeless(models.Model):
     
     def __str__(self):
         return self.name
+
+class yearly_homeless(models.Model):
+    name = models.CharField(max_length=250, defaults="")
+    year = models.DecimalField(max_digits=4, decimal_places=0)
+    total = models.DecimalField(max_digits=9, decimal_places=0)
+    
+    def __str__(self):
+        return self.name
