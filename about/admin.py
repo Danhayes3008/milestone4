9ee3dab12ab0b5b644a96de93f510744b578d9c0
danhayes <danhayes3008@outlook.com>
2019-12-06 12:17:38 +0000

    added the register for yearly homeless

--- about/admin.py
+++ about/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import total_homeless
+from .models import total_homeless, yearly_homeless
 
 admin.site.register(total_homeless)
+admin.site.register(yearly_homeless)
