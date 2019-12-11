e1430baec74dc338853f7beba0a359360f9f7efa
danhayes <danhayes3008@outlook.com>
2019-12-11 14:36:18 +0000

    rewrote the file to seperate the different things needing testing as the setUp was causing problems with some of it

--- users/test_views.py
+++ users/test_views.py
@@ -1,18 +1,25 @@
 from django.test import TestCase
 from django.shortcuts import get_list_or_404
+from django.contrib.auth.models import User
 
-class TestViews(TestCase):
+class TestLogin(TestCase):
     def test_get_login(self):
         page = self.client.get("/users/login/")
         self.assertEqual(page.status_code, 200)
         self.assertTemplateUsed(page, "login.html")
-        
-    def test_registration_page(self):
-        page = self.client.get("/users/register/")
-        self.assertEqual(page.status_code, 200)
-        self.assertTemplateUsed(page, "registration.html")
-        
+
+class TestProfile(TestCase):
+    def setUp(self):
+        user = User.objects.create_user(username="test", password="testing", email="testing@outlook.com")
+        self.client.login(username="test", password="testing")
+                    
     def test_profile_page(self):
         page = self.client.get("/users/profile/")
         self.assertEqual(page.status_code, 200)
         self.assertTemplateUsed(page, "profile.html")
+        
+class TestRegistration(TestCase):
+    def test_registration_page(self):
+        page = self.client.get("/users/register/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "registration.html")
