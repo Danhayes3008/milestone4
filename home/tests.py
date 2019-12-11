8e75a60a8e1bc8b482b760cedfd46b5898d44c9e
danhayes <danhayes3008@outlook.com>
2019-12-11 11:32:22 +0000

    made the test that checks if the page loads

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+class TesHome(TestCase):
+    def test_index_page(self):
+        page = self.client.get("/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "index.html")
