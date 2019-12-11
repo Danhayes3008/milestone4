d09d07e86d92a8d77b47c7ebd874909b1175fd6c
danhayes <danhayes3008@outlook.com>
2019-12-11 11:57:38 +0000

    working on the test for the profile page. getting 1 error saying object has no attribute 'email'

--- users/test_views.py
+++ users/test_views.py
@@ -11,3 +11,8 @@ class TestViews(TestCase):
         page = self.client.get("/users/register/")
         self.assertEqual(page.status_code, 200)
         self.assertTemplateUsed(page, "registration.html")
+        
+    def test_profile_page(self):
+        page = self.client.get("/users/profile/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "profile.html")
