cf116692ab67d2d37844de577e71fa0bd65c6f40
danhayes <danhayes3008@outlook.com>
2019-12-11 16:19:40 +0000

    started the test registration form class by adding the setUp def and getting the clean email test to pass

--- users/test_forms.py
+++ users/test_forms.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 
 class TestLoginForm(TestCase):
     def setUp(self):
@@ -10,3 +10,12 @@ class TestLoginForm(TestCase):
     def test_login_form(self):
         form = LoginForm({'username': 'test'}, {'password': 'testing'})
         self.assertFalse(form.is_valid())
+        
+class TestRegistrationForm(TestCase):
+    def setUp(self):
+        user = User.objects.create_user(username="test", password="testing", email="testing@outlook.com")
+        self.client.login(username="test", password="testing")
+    
+    def test_clean_email(self):
+        email = RegistrationForm({'username': 'test'}, {'password': 'testing'}, {'email': 'testing@outlook.com'})
+        self.assertFalse(email.is_valid())
