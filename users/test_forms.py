220e1cc6a52be08308b0acfa8af93d529d407137
danhayes <danhayes3008@outlook.com>
2019-12-13 14:12:09 +0000

    worked on the TestRegistrationForm class and added theimort get from webbrowser

--- users/test_forms.py
+++ users/test_forms.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from .forms import LoginForm, RegistrationForm
+from webbrowser import get
 
 class TestLoginForm(TestCase):
     def setUp(self):
@@ -13,9 +14,23 @@ class TestLoginForm(TestCase):
         
 class TestRegistrationForm(TestCase):
     def setUp(self):
-        user = User.objects.create_user(username="test", password="testing", email="testing@outlook.com")
+        user = User.objects.create_user(username="test", password="testing", 
+                                        email="testing@outlook.com")
         self.client.login(username="test", password="testing")
+        
+    def test_users_can_register(self):
+        form = RegistrationForm({'username': 'test'}, {'password': 'testing'}, 
+                                {'email': 'testing@outlook.com'})
+        self.assertFalse(form.is_valid())
     
     def test_clean_email(self):
         email = RegistrationForm({'username': 'test'}, {'password': 'testing'}, {'email': 'testing@outlook.com'})
         self.assertFalse(email.is_valid())
+         
+    def test_email_cant_be_left_blank(self):
+        def setUp(self):
+            user = User.objects.create_user(username="test", password="testing", 
+                                        email="")
+        email = RegistrationForm({'username': 'test', 'password':'testing', 'email': ''})
+        self.assertFalse(email.is_valid())
+        self.assertEqual(email.errors['email'], ['This field is required.'])
