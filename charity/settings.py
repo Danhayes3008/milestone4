ac5915e4566d7ccffd049602fdf7013b80c12f0d
danhayes <danhayes3008@outlook.com>
2019-12-15 11:46:31 +0000

    moved the secret key to a env.py file, should have done this at the begining of the project to avoid it being accesable to people through my github page

--- charity/settings.py
+++ charity/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$!#63i3awnauxe-css0ir86h3=0!3qora-k51@z-c)=c*c8k5t'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
