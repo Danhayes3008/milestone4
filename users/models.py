2858179e14ad7faa69e200b8c1d930499f77e9fb
danhayes <danhayes3008@outlook.com>
2019-12-15 17:14:53 +0000

    added the model class that will allow a user to add information about themselves

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length=254, default='')
+    gender = models.CharField(max_length=6, default='')
+    nationality = models.CharField(max_length=100, default='')
+    birthday=models.DateField(auto_now=False, null=True, blank=True)
+
+    def __str__(self):
+        return self.Profile
