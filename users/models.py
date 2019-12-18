956f41b67ce4c2baf1a7c53b7678cf799cdae7ee
danhayes <danhayes3008@outlook.com>
2019-12-18 14:00:07 +0000

    made changes so that all profile imformation in not a required input and added the link to the User database

--- users/models.py
+++ users/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    name = models.CharField(max_length=254, default='')
-    gender = models.CharField(max_length=6, default='')
-    nationality = models.CharField(max_length=100, default='')
+    name = models.CharField(max_length=250, blank=True)
+    gender = models.CharField(max_length=6, blank=True)
+    nationality = models.CharField(max_length=100, blank=True)
     birthday=models.DateField(auto_now=False, null=True, blank=True)
-    image = models.ImageField(upload_to='images')
+    image = models.ImageField(upload_to='images', blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, default=None)
 
     def __str__(self):
-        return self.Profile
+        return self.name
