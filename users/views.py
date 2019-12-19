a6e1547225d88552d2b312b39c8e579818c55068
danhayes <danhayes3008@outlook.com>
2019-12-19 19:54:46 +0000

    added the update profile view

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from users.forms import LoginForm, RegistrationForm
+from users.forms import LoginForm, RegistrationForm, UserForm, ProfileForm
 
 # This def creates the login capability and uses the login form from the forms.py file
 
@@ -61,3 +61,24 @@ def profile(request):
     return render(request, 'profile.html', {"profile": user})
 def update(request):
     return render(request, 'update.html')
+
+@login_required
+# @transaction.atomic
+def update_profile(request):
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = ProfileForm(request.POST, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, _('Your profile was successfully updated!'))
+            return redirect('settings:profile')
+        else:
+            messages.error(request, _('Please correct the error below.'))
+    else:
+        user_form = UserForm(instance=request.user)
+        profile_form = ProfileForm(instance=request.user.profile)
+    return render(request, 'profiles/profile.html', {
+        'user_form': user_form,
+        'profile_form': profile_form
+    })
