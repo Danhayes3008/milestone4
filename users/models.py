c5d3ea71103755aae88a46618f9b7ca9ab14b365
danhayes <danhayes3008@outlook.com>
2019-12-19 19:50:22 +0000

    made changes to make the profile model work the way i need it to

--- users/models.py
+++ users/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from test.dtracedata import instance
 
 class Profile(models.Model):
     name = models.CharField(max_length=250, blank=True)
@@ -10,4 +13,14 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, default=None)
 
     def __str__(self):
-        return self.name
+        return self.user.username
+    
+@receiver(post_save, sender=User)
+def create_or_update_user_profile (sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
