8ee61f98023bdbbfc9116168f9ac1dcd1317e95b
danhayes <danhayes3008@outlook.com>
2019-12-20 20:08:54 +0000

    added the Donations class and DonationslineItem class

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from projects.views import Project
 
 # Create your models here.
+class Donations(models.Model):
+    full_name = models.CharField(max_length=50, blank=False)
+    phone_number = models.CharField(max_length=20, blank=False)
+    country = models.CharField(max_length=40, blank=False)
+    postcode = models.CharField(max_length=20, blank=True)
+    town_or_city = models.CharField(max_length=40, blank=False)
+    streetadress1 = models.CharField(max_length=40, blank=False)
+    streetaddress2 = models.CharField(max_length=40, blank=False)
+    county = models.CharField(max_length=40, blank=False)
+    date = models.DateField()
+    
+    def __str__(self):
+        return "{0}-{1}-{2}".format(self.id, self.date, self.full_name)
+        
+class DonationsLineItem(models.Model):
+    donations = models.ForeignKey(Donations, null=False)
+    project = models.ForeignKey(Project, null=False)
+    quantity = models.IntegerField(blank=False)
+    
+    def __str__(self):
+        return "{0} {1} @ {2}".format(self.quantity, self.project.name, self.project.price)
