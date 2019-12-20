33b4495d3841a6a5112e5debbbd802be06864127
danhayes <danhayes3008@outlook.com>
2019-12-20 13:21:52 +0000

    used the view from the lesson material as a template to get my views to work. some ajustments have been made to make the code work with my project

--- donate/views.py
+++ donate/views.py
@@ -1,29 +1,38 @@
 from django.shortcuts import render, redirect, reverse
 
+# Create your views here.
 def view_donate(request):
+    """A View that renders the cart contents page"""
     return render(request, "donate.html")
 
+
 def amount_to_donate(request, id):
-    total = int(request.POST.get('total'))
-    
-    donate = request.session.get('cart', {})
-    if id in donate:
-        donate[id] = int(donate[id]) + total
+    """Add a quantity of the specified product to the cart"""
+    amount = int(request.POST.get('amount'))
+
+    cart = request.session.get('cart', {})
+    if id in cart:
+        cart[id] = int(cart[id]) + amount      
     else:
-        donate[id] = donate.get(id, total)
-    
-    request.session['donate'] = donate
-    return redirect(reverse('index'))
+        cart[id] = cart.get(id, amount) 
+
+    request.session['cart'] = cart
+    return redirect(reverse('donation'))
+
 
 def adjust_donation(request, id):
+    """
+    Adjust the quantity of the specified product to the specified
+    amount
+    """
     print(request.POST)
-    total = int(request.POST.get('total'))
-    donate = request.session.get('donate', {})
-    
-    if total > 0:
-        donate[id] = total
+    amount = int(request.POST.get('amount'))
+    cart = request.session.get('cart', {})
+
+    if amount > 0:
+        cart[id] = amount
     else:
-        donate.pop(id)
+        cart.pop(id)
     
-    request.session['donate'] = donate
-    return redirect(reverse('view_donate'))
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
