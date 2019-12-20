0529f00e3e08f8820bad2352d96b7a817a557de4
danhayes <danhayes3008@outlook.com>
2019-12-20 20:19:29 +0000

    started work on the views for the payment page

--- payment/views.py
+++ payment/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms  import MakePaymentForm, DonationsForm
+from .models import DonationsLineItem
+from django.conf import settings 
+from django.utils import timezone
+from projects.models import Product
+import stripe
 
 # Create your views here.
+stripe.api_key = settings.STRIPE_SECRET
+
 def payment(request):
     return render(request, 'payment.html')    
