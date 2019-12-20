642981e70d4cc0e0088c05af2b68ff4de689300a
danhayes <danhayes3008@outlook.com>
2019-12-20 20:12:31 +0000

    updated the admin.py so that payments can be made

--- payment/admin.py
+++ payment/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import DonationsLineItem, Donations
 
 # Register your models here.
+class DonationsLineAdminInline(admin.TabularInline):
+    model = DonationsLineItem
+    
+class DonationsAdmin(admin.ModelAdmin):
+    inlines = (DonationsLineAdminInline, )
+    
+admin.site.register(Donations, DonationsAdmin)
