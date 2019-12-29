74ff507ebf06b25d131952a316e8e19fbe58afdb
danhayes <danhayes3008@outlook.com>
2019-12-29 15:34:39 +0000

    removed the training_cart_contents section, and removed the imports for support and training

--- donate/contexts.py
+++ donate/contexts.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from projects.models import Housing, Training, Support
+from projects.models import Housing
 
 def housing_cart_contents(request):
     """
@@ -16,23 +16,5 @@ def housing_cart_contents(request):
         total += amount * housing.donation
         project_count += amount
         cart_items.append({'id':id, 'amount':amount, 'housing':housing})
-        
-    return {'cart_items': cart_items, 'total':total, 'project_count':project_count}
-
-def training_cart_contents(request):
-    """
-    Ensures that the cart contents are available when rendering every page
-    """
-    
-    cart = request.session.get('cart', {})
-    
-    cart_items = []
-    total = 0
-    project_count = 0
-    for id, amount in cart.items():
-        training = get_object_or_404(Training, pk=id)
-        total += amount * training.donation
-        project_count += amount
-        cart_items.append({'id':id, 'amount':amount, 'training':training})
         
     return {'cart_items': cart_items, 'total':total, 'project_count':project_count}
