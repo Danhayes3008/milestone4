1af87bc5dee94cc5c55688c1dc4b47f1d873a267
danhayes <danhayes3008@outlook.com>
2019-12-29 17:42:08 +0000

    changed the model name

--- payment/models.py
+++ payment/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from projects.models import Housing
+from projects.models import Donations
 
 # Create your models here.
-class Donations(models.Model):
+class Donation(models.Model):
     full_name = models.CharField(max_length=50, blank=False)
     phone_number = models.CharField(max_length=20, blank=False)
     country = models.CharField(max_length=40, blank=False)
@@ -17,9 +17,9 @@ class Donations(models.Model):
         return "{0}-{1}-{2}".format(self.id, self.date, self.full_name)
         
 class DonationsLineItem(models.Model):    
-    donations = models.ForeignKey(Donations, null=False)
-    housing = models.ForeignKey(Housing, null=False)    
+    donation = models.ForeignKey(Donation, null=False)
+    donations = models.ForeignKey(Donations, null=False)    
     quantity = models.IntegerField(blank=False)
     
     def __str__(self):
-        return "{0} {1} @ {2}".format(self.quantity, self.housing.name, self.housing.price)
+        return "{0} {1} @ {2}".format(self.quantity, self.donations.name, self.donations.price)
