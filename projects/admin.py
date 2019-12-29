d3f35cb65b61d53239aeed091b24e8daee452733
danhayes <danhayes3008@outlook.com>
2019-12-29 14:23:03 +0000

    removed the register for the support model as it was not needed any more

--- projects/admin.py
+++ projects/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Housing, Training, Support
+from .models import Housing, Training
 
 admin.site.register(Housing)
 admin.site.register(Training)
-admin.site.register(Support)
