7d7093e4664bb040286da386d19e92df0048442d
danhayes <danhayes3008@outlook.com>
2019-12-29 15:19:55 +0000

    altered the housing class to the new model for donations

--- projects/models.py
+++ projects/models.py
@@ -2,17 +2,9 @@ from django.db import models
 
 class Housing(models.Model):
     name = models.CharField(max_length=254, default='')
-    category = models.CharField(max_length=150, default='')
-    description = models.TextField()
     donation = models.DecimalField(max_digits=9, decimal_places=2, default='1.00')
+    price = models.DecimalField(max_digits=9, decimal_places=2, default='1.00')
     target = models.DecimalField(max_digits=9, decimal_places=2, default='1.00')
-    image = models.ImageField(upload_to='images')
-    AddressLine1 = models.CharField(max_length=50, default='')
-    AddressLine2 = models.CharField(max_length=50, default='')
-    town_or_city = models.CharField(max_length=50, default='')
-    county = models.CharField(max_length=50, default='')
-    country = models.CharField(max_length=50, default='')
-    postcode = models.CharField(max_length=20, default='')
     
     def __str__(self):
         return self.name
