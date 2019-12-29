5cbc3c9d260260d8b0b9902c8040c66b209d3000
danhayes <danhayes3008@outlook.com>
2019-12-29 15:47:34 +0000

    changed the housing class name to donations

--- projects/models.py
+++ projects/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Housing(models.Model):
+class Donations(models.Model):
     name = models.CharField(max_length=254, default='')
     donation = models.DecimalField(max_digits=9, decimal_places=2, default='1.00')
     price = models.DecimalField(max_digits=9, decimal_places=2, default='1.00')
