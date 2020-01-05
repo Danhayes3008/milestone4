e0f62473cab8b9d03c600d76159d0a349d22bc21
danhayes <danhayes3008@outlook.com>
2020-01-05 15:34:20 +0000

    added the content needed for my custom_storages.py file to work

--- charity/settings.py
+++ charity/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'donate',
     'contact',
     'payment',
-    'storages'
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -141,7 +141,8 @@ AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3BotoStorage'
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
@@ -149,8 +150,11 @@ STATICFILES_DIRS = (
 )
 
 MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+
 
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
